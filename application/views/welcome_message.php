f743e8a5d0b36610990d8318d78a59077fb58fe1
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2017-04-10 04:32:01 -0700

    Upload Site

---
+++ application/views/welcome_message.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Portofolio</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css">
+</head>
+<body>
+	<div class="navigation">
+			<div class="container">
+				<ul>
+					<a href="#"></ABBR></ABBR><li>Home</li></a>
+					<a href="#portofolio"><li>Portofolio</li></a>
+					<a href="#about"><li>About</li></a>
+				</ul>
+			</div>
+		</div>
+	<div class="container">
+		<div class="foto">
+			<img src="https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/16507834_1511529205554200_3843576929689901237_n.jpg?oh=211770a39edc7a04ab941bdbd5413d21&oe=5956A0A8">
+		</div>
+		<div class="page">
+			<div class="head">
+				<h1>Irvan Ariyanto</h1>
+				<h4>Analyst - Developer - Scientist</h4>
+			</div>
+			<div class="sosmed">
+				<div class="row">
+					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><a href="mailto:irvan.ariyanto104@gmail.com" target="_blank"><img src="<?php echo base_url();?>assets/img/google.png" alt="Gmail"></a></div>
+					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><a href="http://github.com/irvanariyanto" target="_blank"><img src="<?php echo base_url();?>assets/img/github.png" alt="Github"></a></div>
+					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><a href="https://www.linkedin.com/in/irvanariyanto/" target="_blank"><img src="<?php echo base_url();?>assets/img/in.png" alt="Linkedin"></a></div>
+					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3"><a href="https://www.facebook.com/ariyancliq" target="_blank"><img src="<?php echo base_url();?>assets/img/facebook.png" alt="Facebook"></a></div>
+				</div>
+			</div>
+			<div class="work" id="portofolio">
+				<h2 class="judul">Portofolio</h2>
+				<h5>Website</h5>
+				<div class="row">
+					<div class="col-lg-4 col-md-4 "><a href="http://www.cahayagrafika.com/"><img src="<?php echo base_url();?>assets/img/cahaya.png"></a></div>
+					<div class="col-lg-4 col-md-4"><img src="<?php echo base_url();?>assets/img/cms.png"></div>
+					<div class="col-lg-4 col-md-4"><a href="http://insankamilsidoarjo.sch.id/"><img src="<?php echo base_url();?>assets/img/insan.png"></a></div>
+				</div>
+				<br>
+				<h5>Mobile Apps</h5>
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><img src="<?php echo base_url();?>assets/img/kamus.png"></div>
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><img src="<?php echo base_url();?>assets/img/profile shelter.jpg"></div>
+					<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><img src="<?php echo base_url();?>assets/img/Tiket.png"></div>
+				</div>
+
+			</div>
+			<div class="study">
+				<h2 class="judul">Education</h2>
+				<div class="row">
+					<div class="col-lg-3 col-md-3 col-sm-3"></div>
+					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-4">
+						<p>2002 - 2008</p>
+						<p>2008 - 2011</p>
+						<p>2011 - 2013</p>
+						<p>2013 - sekarang</p>
+					</div>
+					<div class="col-lg-2 col-md-2 col-sm-2 col-xs-4">
+						<p>-</p>
+						<p>-</p>
+						<p>IPA</p>
+						<p>T. Informatika</p>
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-3 col-xs-4">
+						<p>SDN JEBLOGAN 3</p>
+						<p>MTsN 1 PARON</p>
+						<p>MAN 2 MADIUN</p>
+						<p>UIN Maulana Malik Ibrahim Malang</p>
+					</div>
+					<div class="col-lg-2 col-md-2 col-sm-2"></div>
+				</div>
+			</div>
+			<div class="about" id="about">
+				<h2 class="judul">About</h2>
+				<p>I am a student of class 2013 of UIN Maulana Malik Ibrahim Malang. I am actively contribute to the development of open source projects that can be seen in mygithub and currently focused on research of network in ETH0 community..</p>
+			</div>
+			<div class="do">
+				<h2 class="judul">What I Do</h2>
+				<div class="row">
+					<div class="col-lg-4 col-md-4 col-sm-4"><img src="<?php echo base_url();?>assets/img/consulting.png"><br><label><em>Analysis</em></label></div>
+					<div class="col-lg-4 col-md-4 col-sm-4"><img src="<?php echo base_url();?>assets/img/mobile.png"><br><label><em>Android Developer</em></label></div>
+					<div class="col-lg-4 col-md-4 col-sm-4"><img src="<?php echo base_url();?>assets/img/network.png"><br><label><em>Network Engineer</em></label></div>
+				</div>
+			</div>
+		</div>
+		<div class="footer">
+			&copy; Irvan Ariyanto
+		</div>
+	</div>
+</body>
+</html>
