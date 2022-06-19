20eb76e71e14d6fcd0efd51387f783f3fa75fad2
GDP-IrvanAriyanto <irvan.ariyanto@gdplabs.id>
2022-06-19 20:21:12 +0700

    add home section

---
+++ include.js
@@ -0,0 +1,47 @@
+$(function () {
+  $(".typed").typed({
+    strings: ["Software Developer Engineer"],
+    stringsElement: null,
+    typeSpeed: 30,
+    startDelay: 1200,
+    backSpeed: 20,
+    backDelay: 500,
+    loop: true,
+    loopCount: false,
+    showCursor: false,
+    cursorChar: "|",
+    attr: null,
+    contentType: "html",
+    callback: function () {},
+    preStringTyped: function () {},
+    onStringTyped: function () {},
+    resetCallback: function () {},
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".splide", {
+    direction: "ttb",
+    height: "35rem",
+    autoWidth: true,
+    wheel: true,
+  });
+  splide.mount();
+});
+
+// document.addEventListener( 'DOMContentLoaded', function() {
+//   var splide = new Splide( '.splide_image', {
+//     direction: 'ttb',
+//     height: '50rem',
+//     autoWidth: true,
+//     wheel: true,
+//   });
+//   splide.mount();
+// } );
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#image-slider", {
+    cover: true,
+    heightRatio: 1.3,
+  }).mount();
+});
